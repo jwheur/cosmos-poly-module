3a976178575f001be15145b96343924e6cdca561
Ivan Poon <ravenxce@gmail.com>
2021-05-18 17:29:45 +0800

    Fix err msg

--- ccm/internal/types/genesis.go
+++ ccm/internal/types/genesis.go
@@ -62,7 +62,7 @@ func ValidateGenesis(data GenesisState) error {
 	}
 
 	if data.CreatedTxCount != sdk.NewInt(int64(len(data.CreatedTxDetails))) {
-		return fmt.Errorf("number of CreatedTxDetails should match CreatedTxCount in ccm genesis, is %d but expected %d", len(data.CreatedTxDetails), data.CreatedTxCount)
+		return fmt.Errorf("number of CreatedTxDetails should match CreatedTxCount in ccm genesis, is %d but expected %s", len(data.CreatedTxDetails), data.CreatedTxCount.String())
 	}
 
 	return nil
